b84595b675980768a1fcc93218666145a3081432
Pasi Niemi <pasi.niemi@nitorcreations.com>
2016-12-20 14:13:41 +0200

    Tested version of log sending (no batching so slightly inefficient)

--- n_utils/cf_utils.py
+++ n_utils/cf_utils.py
@@ -17,6 +17,8 @@ import boto3
 import time
 import os
 import json
+import requests
+from requests.exceptions import ConnectionError
 
 class InstanceInfo(object):
     _info = None
@@ -26,14 +28,21 @@ class InstanceInfo(object):
     initialStatus = ""
     def __init__(self):
         if os.path.isfile('/opt/nitor/instance-data.json'):
-            self._info = json.loads(open('/opt/nitor/instance-data.json'))
+            try:
+                self._info = json.loads(open('/opt/nitor/instance-data.json'))
+            except:
+                pass
         if os.path.isfile('C:/nitor/instance-data.json'):
-            self._info = json.loads(open('C:/nitor/instance-data.json'))
+            try:
+                self._info = json.loads(open('C:/nitor/instance-data.json'))
+            except:
+                pass
         if not self._info:
             try:
                 response = requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document')
                 self._info = json.loads(response.text)
                 self.instanceId = self._info['instanceId']
+                os.environ['AWS_DEFAULT_REGION'] = self._info['region']
                 ec2 = boto3.client('ec2')
                 tags = {}
                 tagResponse = ec2.describe_tags(Filters=
@@ -50,24 +59,28 @@ class InstanceInfo(object):
                     clf = boto3.client('cloudformation')
                     stacks = clf.describe_stacks(StackName=self.stackName)
                     stackParameters = {}
-                    for parameter in  stacks['Stacks'][0]['Parameters']:
-                        stackParameters[paramater['ParameterKey']] = parameter['ParamaterValue']
+                    for parameter in stacks['Stacks'][0]['Parameters']:
+                        stackParameters[parameter['ParameterKey']] = parameter['ParameterValue']
                     for output in  stacks['Stacks'][0]['Outputs']:
                         stackParameters[output['OutputKey']] = output['OutputValue']
                     self._info['StackData'] = stackParameters
-                    self._info['FullStackData'] = stacks['Stacks'][0]
             except ConnectionError:
                 self._info = {}
             infoFile = None
             if os.path.isdir('C:/'):
                 if not os.path.isdir('C:/nitor'):
                     os.makedirs('C:/nitor')
-                    infoFile = 'C:/nitor/instance-data.json'
+                infoFile = 'C:/nitor/instance-data.json'
             elif not os.path.isdir('/opt/nitor'):
                 os.makedirs('/opt/nitor')
+           if os.path.isdir('/opt/nitor'):
                 infoFile = '/opt/nitor/instance-data.json'
             with open(infoFile, 'w') as outf:
                 outf.write(json.dumps(self._info))
+       if 'instanceId' in self._info:
+               self.instanceId = self._info['instanceId']
+       if 'region' in self._info:
+               os.environ['AWS_DEFAULT_REGION'] = self._info['region']
         if 'FullStackData' in self._info and 'StackStatus' in self._info['FullStackData']:
             self.initialStatus = self._info['FullStackData']['StackStatus']
         if 'Tags' in self._info:
@@ -97,20 +110,28 @@ class LogSender(object):
         except:
             pass
         streamDesc = self._logs.describe_log_streams(logGroupName=self.groupName,
-                                                     logStreamName=self.streamName)
+                                                     logStreamNamePrefix=self.streamName)
         self.token = None
         if 'uploadSequenceToken' in streamDesc['logStreams'][0]:
             self.token = streamDesc['logStreams'][0]['uploadSequenceToken']
     def send(self, line):
         events = []
+       message = line.decode('utf-8','ignore').rstrip()
         event = {}
-        event['timestamp'] = time.time()
-        event['message'] = line
+        event['timestamp'] = int(time.time() * 1000)
+        event['message'] = message
         events.append(event)
-        logResponse = self._logs.put_log_events(logGroupName=self.groupName,
-                                                logStreamName=self.streamName,
-                                                logEvents=events,
-                                                sequenceToken=self.token)
+        if self.token:
+            logResponse = self._logs.put_log_events(logGroupName=self.groupName,
+                                                    logStreamName=self.streamName,
+                                                    logEvents=events,
+                                                    sequenceToken=self.token)
+        else:
+            logResponse = self._logs.put_log_events(logGroupName=self.groupName,
+                                                    logStreamName=self.streamName,
+                                                    logEvents=events)
+        if 'CLOUDWATCH_LOG_DEBUG' in os.environ:
+            print "Sent " + message + " to " + self.streamName
         self.token = logResponse['nextSequenceToken']
 
 def send_logs_to_cloudwatch(fileName):
