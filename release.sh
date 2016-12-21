6a88ed19c1bfdefae6cf350aa8e5f600acff0e9d
Pasi Niemi <pasi.niemi@nitorcreations.com>
2016-12-21 14:02:15 +0200

    Add major releases to release script

--- release.sh
+++ release.sh
@@ -3,7 +3,13 @@
 VERSION=$(grep version setup.py | cut -d\' -f 2)
 MAJOR=${VERSION//.*}
 MINOR=${VERSION##*.}
-MINOR=$(($MINOR + 1))
+if [ "$1" = "-m" ]; then
+  MAJOR=$(($MAJOR + 1))
+  MINOR="0"
+  shift
+else
+  MINOR=$(($MINOR + 1))
+fi
 sed -i "s/$VERSION/$MAJOR.$MINOR/g" setup.py
 git commit -m "$1" setup.py
 git tag "$MAJOR.$MINOR" -m "$1"
