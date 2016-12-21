eef2c0e8ab0e70291b5773cb4627967d133856a0
Pasi Niemi <pasi.niemi@nitorcreations.com>
2016-12-21 12:35:36 +0200

    Next release

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='nitor_deploy_tools',
-      version='0.8',
+      version='0.9',
       description='Utilities for deploying with Nitor aws-utils',
       url='http://github.com/NitorCreations/nitor-deploy-tools',
-      download_url = 'https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.8',
+      download_url = 'https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.9',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
