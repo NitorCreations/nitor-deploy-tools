29ce6cb742b0943e6624cd6fa127d3e3c2ff526e
Pasi Niemi <pasi.niemi@nitorcreations.com>
2017-04-04 15:29:26 +0300

    Add dry-run argument to deploy-stack.sh

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@
 from setuptools import setup
 
 setup(name='nitor_deploy_tools',
-      version='0.82',
+      version='0.83',
       description='Utilities for deploying with Nitor aws-utils',
       url='http://github.com/NitorCreations/nitor-deploy-tools',
-      download_url = 'https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.82',
+      download_url = 'https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.83',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
