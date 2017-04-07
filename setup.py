b4c5b74fd585c0e4abbe227581ea5c6874cae307
Pasi Niemi <pasi.niemi@nitorcreations.com>
2017-04-07 11:06:45 +0300

    Setup fixes and a timeout for instance metadata for running outside aws

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@
 from setuptools import setup
 
 setup(name='nitor_deploy_tools',
-      version='0.83',
+      version='0.84',
       description='Utilities for deploying with Nitor aws-utils',
       url='http://github.com/NitorCreations/nitor-deploy-tools',
-      download_url = 'https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.83',
+      download_url = 'https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.84',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
