f0888dc79ef7c97d931cad4cfa78ad374c298c15
Pasi Niemi <pasi.niemi@nitorcreations.com>
2017-04-26 11:53:26 +0300

    Require argcomplete

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ from setuptools import setup
 from n_utils import SCRIPTS, CONSOLESCRIPTS
 
 setup(name='nitor_deploy_tools',
-      version='0.86',
+      version='0.87',
       description='Utilities for deploying with Nitor aws-utils',
       url='http://github.com/NitorCreations/nitor-deploy-tools',
-      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.86',
+      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.87',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
@@ -37,7 +37,8 @@ setup(name='nitor_deploy_tools',
           'pycrypto',
           'requests',
           'termcolor',
-          'ipaddr'
+          'ipaddr',
+          'argcomplete'
       ] + ([
           'win-unicode-console',
           'wmi',
