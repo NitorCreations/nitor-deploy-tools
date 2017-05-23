980388fbff90a464a6c6566a9e3b14cbe9560a46
Pasi Niemi <pasi.niemi@nitorcreations.com>
2017-05-23 08:11:42 +0300

    Move functionality for generating jobs from deploytools to the generating script

--- bin/list-jobs.sh
+++ bin/list-jobs.sh
@@ -3,8 +3,12 @@
 if [ "$_ARGCOMPLETE" ]; then
   exit 0
 fi
-
+IGNORE_PROPS='^(BASH|DIR|TERM|.?UID|_|GROUPS|IFS|OPTERR|OPTIND|SHELL|PIPE|PS4|PPID|SHLVL|PATH|PWD|OSTYPE|HOST(NAME|TYPE)|MACHTYPE)'
 source $(n-include autocomplete-helpers.sh)
+#If region not set in infra files, get the region of the instance or from env
+[ "$REGION" ] || REGION=$(ec2-region)
+# Same logic as above for account id
+[ "$ACCOUNT_ID" ] || ACCOUNT_ID=$(account-id)
 
 buildable_branches() {
   for BRANCH in $(git branch -r | grep -v origin/HEAD | cut -d/ -f 2-); do
@@ -18,31 +22,23 @@ buildable_branches() {
   done
 }
 list_jobs() {
+  mkdir -p job-properties
   for GIT_BRANCH in $(buildable_branches); do 
     cd "$GIT_BRANCH-checkout"
     for IMAGE_DIR in $(get_bakeable_images); do
-      source source_infra_properties.sh $IMAGE_DIR
-      if ! [ "$JENKINS_JOB_NAME" ]; then
-        JENKINS_JOB_NAME="${JENKINS_JOB_PREFIX}-${IMAGE_DIR}-bake"
-      fi
-      if ! [ "$SKIP_BAKE_JOB" = "y" ]; then
-        if ! [ "$MANUAL_DEPLOY" = "y" ]; then
-          MANUAL_DEPLOY="n"
-        fi
-        echo "$GIT_BRANCH:image:$IMAGE_DIR:$MANUAL_DEPLOY:$JENKINS_JOB_PREFIX:$JENKINS_JOB_NAME"
-        unset JENKINS_JOB_NAME
-      fi
+      TMP=$(mktemp -p .)
+      env -i REGION="$REGION" ACCOUNT_ID="$ACCOUNT_ID" GIT_BRANCH="$GIT_BRANCH" \
+        bash -c "source source_infra_properties.sh $IMAGE_DIR ; set" | egrep -v "$IGNORE_PROPS" > $TMP 
+      mv $TMP ../job-properties/image-$IMAGE_DIR.properties
+      echo "$IMAGE_DIR:$GIT_BRANCH:image:-"
     done
     for IMAGE_DIR in $(get_stack_dirs); do
       for STACK in $(get_stacks $IMAGE_DIR); do
-        if ! [ "$JENKINS_JOB_NAME" ]; then
-          JENKINS_JOB_NAME="${JENKINS_JOB_PREFIX}-${IMAGE_DIR}-${STACK}-deploy"
-        fi
-        source source_infra_properties.sh $IMAGE_DIR $STACK
-        if ! [ "$SKIP_DEPLOY_JOB" = "y" ]; then
-          echo "$GIT_BRANCH:stack:$IMAGE_DIR:$STACK:$JENKINS_JOB_PREFIX:$JENKINS_JOB_NAME"
-        fi
-        unset JENKINS_JOB_NAME
+        TMP=$(mktemp -p .)
+        env -i REGION="$REGION" ACCOUNT_ID="$ACCOUNT_ID" GIT_BRANCH="$GIT_BRANCH" \
+          bash -c "source source_infra_properties.sh $IMAGE_DIR $STACK; set" | egrep -v "$IGNORE_PROPS" > $TMP 
+        mv $TMP ../job-properties/stack-$IMAGE_DIR-$STACK.properties
+        echo "$IMAGE_DIR:$GIT_BRANCH:stack:$STACK"
       done
     done
     cd ..
