49080bc600eab6acf444baceaa6c0460572172ee
Pasi Niemi <pasi.niemi@nitorcreations.com>
2017-05-30 15:51:59 +0300

    Request syntax fix for image sharing

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ from setuptools import setup
 from n_utils import SCRIPTS, CONSOLESCRIPTS
 
 setup(name='nitor_deploy_tools',
-      version='0.122',
+      version='0.123',
       description='Utilities for deploying with Nitor aws-utils',
       url='http://github.com/NitorCreations/nitor-deploy-tools',
-      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.122',
+      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.123',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
