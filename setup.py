f830b40d1dccac82d8c250b8f93772ddb992c17c
Pasi Niemi <pasi.niemi@nitorcreations.com>
2017-06-14 18:34:10 +0300

    Fix ec2-get-tag output typo

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ from setuptools import setup
 from n_utils import SCRIPTS, CONSOLESCRIPTS
 
 setup(name='nitor_deploy_tools',
-      version='0.133',
+      version='0.134',
       description='Utilities for deploying with Nitor aws-utils',
       url='http://github.com/NitorCreations/nitor-deploy-tools',
-      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.133',
+      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.134',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
