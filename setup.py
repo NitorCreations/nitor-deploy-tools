c3039d049727a1086cc191244ccf390cfb8b3b72
Pasi Niemi <pasi.niemi@nitorcreations.com>
2017-07-16 12:17:05 +0300

    Add env variable DEPLOY_ROLE_ARN to be able to specify a role to assume per stack

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ from setuptools import setup
 from n_utils import SCRIPTS, CONSOLESCRIPTS
 
 setup(name='nitor_deploy_tools',
-      version='0.139',
+      version='0.140',
       description='Utilities for deploying with Nitor aws-utils',
       url='http://github.com/NitorCreations/nitor-deploy-tools',
-      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.139',
+      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.140',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
