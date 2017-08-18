3dbecdfc8323db00650c7fb5264cede743831b0f
Pasi Niemi <pasi.niemi@nitorcreations.com>
2017-08-18 11:09:15 +0300

    Fix for yaml intrisinc functions

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ from setuptools import setup
 from n_utils import SCRIPTS, CONSOLESCRIPTS
 
 setup(name='nitor_deploy_tools',
-      version='0.159',
+      version='0.160',
       description='Utilities for deploying with Nitor aws-utils',
       url='http://github.com/NitorCreations/nitor-deploy-tools',
-      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.159',
+      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/0.160',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
