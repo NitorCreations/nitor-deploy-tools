d0fdb758091259af6fcc2ac94c4603fb899d6839
Pasi Niemi <pasi.niemi@nitorcreations.com>
2018-01-25 13:31:46 +0200

    Make the repo-defined bake role parameter separate from the deploy role because cross-account deployment normally uses local role for baking and a remote one for deploying

--- bin/bake-image.sh
+++ bin/bake-image.sh
@@ -59,8 +59,8 @@ cache () {
 }
 
 #If assume-deploy-role.sh is on the path, run it to assume the appropriate role for deployment
-if [ -n "$DEPLOY_ROLE_ARN" ] && [ -z "$AWS_SESSION_TOKEN" ]; then
-  eval $(ndt assume-role $DEPLOY_ROLE_ARN)
+if [ -n "$BAKE_ROLE_ARN" ] && [ -z "$AWS_SESSION_TOKEN" ]; then
+  eval $(ndt assume-role $BAKE_ROLE_ARN)
 elif which assume-deploy-role.sh > /dev/null && [ -z "$AWS_SESSION_TOKEN" ]; then
   eval $(assume-deploy-role.sh)
 fi
