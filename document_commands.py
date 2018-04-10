fabfe9171cd6f4bc897005a7795a3ad39c1599af
Pasi Niemi <pasi.niemi@nitorcreations.com>
2018-04-10 15:48:48 +0300

    python 3 fix

--- document_commands.py
+++ document_commands.py
@@ -3,12 +3,13 @@
 from __future__ import print_function
 from n_utils import COMMAND_MAPPINGS, PATH_COMMANDS, NDT_AND_CONSOLE
 from subprocess import Popen, PIPE
-
+import locale
+SYS_ENCODING = locale.getpreferredencoding()
 CONSOLE_PREFERRED = sorted([cmd.split("=")[0] for cmd in NDT_AND_CONSOLE])
 def do_call(command):
     proc = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     output, err = proc.communicate()
-    return (output + err).strip().replace("'", "\\'")
+    return (output + err).decode(SYS_ENCODING).strip().replace("'", "\\'")
 
 for command in sorted(COMMAND_MAPPINGS.keys()):
     if command in CONSOLE_PREFERRED:
