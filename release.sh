0404356cd725683b645bf9460eb6f0606439f841
Pasi Niemi <pasi.niemi@nitorcreations.com>
2018-04-11 12:37:12 +0300

    Add docker to release process

--- release.sh
+++ release.sh
@@ -40,3 +40,7 @@ git push --tags origin master
 
 python setup.py register -r pypi
 python setup.py sdist upload -r pypi --sign
+docker build -t ndt docker
+docker login -u "$(git config docker.username)" -p "$(lpass show --password docker.com)"
+docker tag ndt:latest nitor/ndt:$NEW_VERSION
+docker push nitor/ndt:$NEW_VERSION
