f865ff428e560097547a19d909818fec71fcf2ce
Pasi Niemi <pasi.niemi@nitorcreations.com>
2018-05-08 15:25:15 +0300

    Removed eval set by modernize as unsafe on both pythons. Input unsafe only on python 2

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ from setuptools import setup
 from n_utils import PATH_COMMANDS, CONSOLESCRIPTS
 
 setup(name='nitor_deploy_tools',
-      version='1.0a25',
+      version='1.0a26',
       description='Utilities for deploying with Nitor aws-utils',
       url='http://github.com/NitorCreations/nitor-deploy-tools',
-      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/1.0a25',
+      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/1.0a26',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
