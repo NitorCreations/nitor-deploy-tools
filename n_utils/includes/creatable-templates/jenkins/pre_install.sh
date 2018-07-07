a38e2148320f76eecc7c11db90229b1fef27d42c
Pasi Niemi <pasi.niemi@nitorcreations.com>
2018-07-07 13:15:02 +0300

    Jenkins template fixes

--- n_utils/includes/creatable-templates/jenkins/pre_install.sh
+++ n_utils/includes/creatable-templates/jenkins/pre_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-yum install -y https://rpm.nodesource.com/pub_6.x/el/7/x86_64/nodesource-release-el7-1.noarch.rpm
+yum install -y https://rpm.nodesource.com/pub_8.x/el/7/x86_64/nodesource-release-el7-1.noarch.rpm
 curl http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo > /etc/yum.repos.d/jenkins.repo
 rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
 yum-config-manager --add-repo http://www.nasm.us/nasm.repo
