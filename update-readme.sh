e523887632f21bb055c5892e9fb70486b606a625
Janne Sinivirta <janne.sinivirta@gmail.com>
2018-08-02 13:29:48 +0300

    create command list reference to it's own markdown file

--- update-readme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INSERT_USAGE=$(grep -n  '## Commands$' docs/index.md | cut -d ":" -f1)
-INSERT_USAGE=$(($INSERT_USAGE + 1)) 
-head -$(($INSERT_USAGE)) docs/index.md > README.md
-./document_commands.py >> README.md
-INSERT_USAGE=$(($INSERT_USAGE + 1))
-tail +$(($INSERT_USAGE)) docs/index.md
