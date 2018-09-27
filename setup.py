199a39063b9145030c014d7f2441d653b431e60d
Pasi Niemi <pasi.niemi@nitorcreations.com>
2018-09-27 13:11:36 +0300

    Write out last pycrypto references in favor of Crytodomex

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ from setuptools import setup
 from n_utils import PATH_COMMANDS, CONSOLESCRIPTS
 
 setup(name='nitor_deploy_tools',
-      version='1.26',
+      version='1.27',
       description='Utilities for deploying with Nitor aws-utils',
       url='http://github.com/NitorCreations/nitor-deploy-tools',
-      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/1.26',
+      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/1.27',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
