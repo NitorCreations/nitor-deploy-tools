7e106fe1c99fa8037b53c89c16cd8e77404aabe0
Pasi Niemi <pasi.niemi@nitorcreations.com>
2018-11-03 11:48:21 +0200

    Also optimize nitor-dt-enable-profile

--- bin/optimize-project-env.sh
+++ bin/optimize-project-env.sh
@@ -2,5 +2,8 @@
 
 sudo -H pip install -U nuitka
 ENV_SCRIPT="$(dirname $(dirname $(n-include hook.sh)))/nitor-dt-load-project-env.py"
+PROFILE_SCRIPT="$(dirname $(dirname $(n-include hook.sh)))/nitor-dt-enable-profile.py"
 python -m nuitka --recurse-to=n_utils.project_util --follow-import-to=n_utils.profile_util $ENV_SCRIPT
+python -m nuitka --follow-import-to=n_utils.profile_util $PROFILE_SCRIPT
 sudo cp nitor-dt-load-project-env.bin $(which nitor-dt-load-project-env)
+sudo cp nitor-dt-enable-profile.bin $(which nitor-dt-enable-profile)
