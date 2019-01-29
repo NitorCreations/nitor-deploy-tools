23e7975931d3640c05d750ed5ff40fb7359710a6
Pasi Niemi <pasi.niemi@nitorcreations.com>
2019-01-29 10:20:46 +0200

    Parameter loading needs to resolve region for docker and ami resolving

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ from setuptools import setup
 from n_utils import PATH_COMMANDS, CONSOLESCRIPTS
 
 setup(name='nitor_deploy_tools',
-      version='1.51',
+      version='1.52',
       description='Tools for deploying to AWS via CloudFormation and Serverless framework that support a pull request based workflow',
       url='http://github.com/NitorCreations/nitor-deploy-tools',
-      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/1.51',
+      download_url='https://github.com/NitorCreations/nitor-deploy-tools/tarball/1.52',
       author='Pasi Niemi',
       author_email='pasi@nitor.com',
       license='Apache 2.0',
